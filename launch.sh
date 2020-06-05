3bdb20d3810c7368f5abec3a60052540f6d4f2eb
nobody246 <a.se8seven@gmail.com>
2020-06-05 08:25:51 -0400

    Create launch.sh

---
+++ launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -z $2 ]; then
+    echo "USAGE: launch.sh (message interval in seconds) (postal tracking code)";
+fi
+zz=600;
+if [ ! -z $1 ]; then
+    zz=$1;
+fi
+nohup ./dmon.sh $zz $2 0<&- &>/dev/null;
